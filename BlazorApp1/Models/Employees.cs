022402766760ef8e58f4348dc0a039a447cef577
Chris Tate <xxclearwaterxx@gmail.com>
2024-04-04 08:33:41 -0400

    Update Employees.cs

--- BlazorApp1/Models/Employees.cs
+++ BlazorApp1/Models/Employees.cs
@@ -11,10 +11,9 @@ namespace BlazorApp1.Models
 
         [Required(ErrorMessage = "{0} must be supplied")] public required string FirstName { get; set; }
         [Required(ErrorMessage = "{0} must be supplied")] public required string LastName { get; set; }
-        [Required] public required DateTime HireDate { get; set; }
+        [Required(ErrorMessage = "{0} must be supplied")]  public required DateTime HireDate { get; set; }
 
-        // Navigation property
-        [Required(ErrorMessage = "{0} must be supplied")] public required List<EmployeePhones> EmployeePhones { get; set; }
-        [Required(ErrorMessage = "{0} must be supplied")] public required List<EmployeeAddresses> EmployeeAddresses { get; set; }
+        public List<EmployeePhones>? EmployeePhones { get; set; }
+        public List<EmployeeAddresses>? EmployeeAddresses { get; set; }
     }
 }
