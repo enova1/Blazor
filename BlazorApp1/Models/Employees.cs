71e82dcf023fef181baa74ec8810fef3ec81b8c9
Chris Tate <xxclearwaterxx@gmail.com>
2024-04-09 10:09:51 -0400

    Update Employees.cs

--- BlazorApp1/Models/Employees.cs
+++ BlazorApp1/Models/Employees.cs
@@ -4,16 +4,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace BlazorApp1.Models
 {
     public class Employees
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int EmployeeId { get; set; }
+   {
+       [Key]
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+       public int EmployeeId { get; set; }
 
-        [Required(ErrorMessage = "{0} must be supplied")] public required string FirstName { get; set; }
-        [Required(ErrorMessage = "{0} must be supplied")] public required string LastName { get; set; }
-        [Required(ErrorMessage = "{0} must be supplied")]  public required DateTime HireDate { get; set; }
+       [Required(ErrorMessage = "{0} must be supplied")]
+       [Display(Name = "First Name", Description = "Employee First Name")]
+       [DataType(DataType.Text)]
+       [StringLength(15, MinimumLength = 1, ErrorMessage = "{0} must be between 1 and 15 characters.")]
+       public required string FirstName { get; set; }
 
-        public List<EmployeePhones>? EmployeePhones { get; set; }
-        public List<EmployeeAddresses>? EmployeeAddresses { get; set; }
-    }
+       [Required(ErrorMessage = "{0} must be supplied")]
+       [Display(Name = "Last Name", Description = "Employee Last Name")]
+       [DataType(DataType.Text)]
+       [StringLength(15, MinimumLength = 3, ErrorMessage = "{0} must be between 1 and 15 characters.")]
+       public required string LastName { get; set; }
+
+       [DataType(DataType.DateTime)]
+       [Display(Name = "Hire Date", Description = "Date of the Employee was hired on.")]
+       [Required] public required DateTime HireDate { get; set; }
+
+       // Navigation property
+       public List<EmployeePhones>? EmployeePhones { get; set; }
+       public List<EmployeeAddresses>? EmployeeAddresses { get; set; }
+   }
 }
